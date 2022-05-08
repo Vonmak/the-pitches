7d5ffdefee5e2d0bb183ae12ee8c1e0e682dcd9d
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-08 16:26:33 +0300

    added the role instance in the shell function.

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app, db
-from app.models import User
+from app.models import Role, User
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
 
@@ -13,7 +13,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User )
+    return dict(app = app,db = db,User = User, Role = Role )
 
 
 
