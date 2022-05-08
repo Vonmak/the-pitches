732bc4454eb84090eecd9f7f5f68cfd3b2401f8c
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-08 18:04:01 +0300

    Added a pitch class.

--- app/models.py
+++ app/models.py
@@ -34,4 +34,40 @@ class Role(db.Model):
 
 
     def __repr__(self):
-        return f'User {self.name}'    
+        return f'User {self.name}' 
+    
+    
+class Pitch(db.Model):
+    '''
+    Pitch class to define Pitch Objects
+    '''
+    __tablename__ = 'pitch'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch = db.Column(db.String)
+    category_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref = 'pitch',lazy="dynamic")
+        
+
+    def save_pitch(self):
+        '''
+        Function that saves pitches
+        '''
+        db.session.add(self)
+        db.session.commit()
+    
+    @classmethod
+    def get_all_pitches(cls):
+        '''
+        Function that queries the database and returns all the pitches
+        '''
+        return Pitch.query.all()
+
+    @classmethod
+    def get_pitches_by_category(cls,cat_id):
+        '''
+        Function that queries the database and returns pitches based on the
+        category passed to it
+        '''
+        return Pitch.query.filter_by(category_id= cat_id)   
