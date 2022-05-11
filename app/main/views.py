a9880c3158eb90c0898d45716b561f5022974769
Victor Ondari <vic@Victors-MacBook-Pro.local>
2022-05-11 13:31:33 +0300

    added the upvote and downvote functions.

--- app/main/views.py
+++ app/main/views.py
@@ -1,13 +1,14 @@
 from flask import render_template, request, abort, url_for, redirect
 from . import main
-from ..models import Pitch, User
-from .forms import UpdateProfile, PitchForm
+from ..models import Pitch, User, Comment, downvote, upVote
+from .forms import UpdateProfile, PitchForm, CommentForm
 from .. import db, photos
 from flask_login import login_required, current_user
 
 
 
 @main.route('/', methods = ['GET','POST'])
+# @login_required
 def index():
     '''
     View root page function that returns the index page and its data
@@ -68,7 +69,7 @@ def product():
 
 
 @main.route('/pitches/new/', methods = ['GET','POST'])
-# @login_required
+@login_required
 def new_pitch():
     form = PitchForm()
     # my_upvotes = Upvote.query.filter_by(pitch_id = Pitch.id)
@@ -83,6 +84,20 @@ def new_pitch():
         return redirect(url_for('main.index'))
     return render_template('pitch.html',form=form)
 
+@main.route('/comment/new/<int:pitch_id>', methods = ['GET','POST'])
+def new_comment(pitch_id):
+    form = CommentForm()
+    pitch=Pitch.query.get(pitch_id)
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_comment = Comment(comment = comment, pitch_id = pitch_id)
+        new_comment.save_comment()
+
+        return redirect(url_for('.new_comment', pitch_id= pitch_id))
+
+    all_comments = Comment.query.filter_by(pitch_id = pitch_id).all()
+    return render_template('comments.html', form = form, comment = all_comments, pitch = pitch )
+
 
 @main.route('/user/<uname>')
 def profile(uname):
@@ -94,7 +109,7 @@ def profile(uname):
     return render_template("profile/profile.html", user = user)
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
-# @login_required
+@login_required
 def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
@@ -113,7 +128,7 @@ def update_profile(uname):
     return render_template('profile/update.html',form =form)
 
 @main.route('/user/<uname>/update/pic', methods=['POST'])
-# @login_required
+@login_required
 def update_pic(uname):
     user = User.query.filter_by(username=uname).first()
     if 'photo' in request.files:
@@ -124,3 +139,33 @@ def update_pic(uname):
     return redirect(url_for('main.profile', uname=uname))
 
 
+@main.route('/pitch/upvote/<int:pitch_id>/upvote', methods = ['GET', 'POST'])
+# @login_required
+def up_vote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    
+    pitch_upvotes = upVote.query.filter_by(pitch_id= pitch_id)
+    
+    if upVote.query.filter(upVote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.index'))
+
+
+    new_upvote = upVote(pitch_id=pitch_id)
+    new_upvote.save_upvotes()
+    return redirect(url_for('main.index'))
+
+
+
+@main.route('/pitch/downvote/<int:pitch_id>/downvote', methods = ['GET', 'POST'])
+# @login_required
+def down_vote(pitch_id):
+    pitch = Pitch.query.get(pitch_id)
+    pitch_downvotes = downvote.query.filter_by(pitch_id= pitch_id)
+    
+    if downvote.query.filter(downvote.pitch_id==pitch_id).first():
+        return  redirect(url_for('main.new_pitch'))
+
+
+    new_downvote = downvote(pitch_id=pitch_id)
+    new_downvote.save_downvotes()
+    return redirect(url_for('main.new_pitch', pitch_id=pitch_id))
